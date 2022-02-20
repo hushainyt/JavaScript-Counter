63c3233539e066f4200e0144a5b38b1ca241f5e8
JAKIR  HUSAIN <98020304+hushainyt@users.noreply.github.com>
2022-02-20 15:22:02 +0530

    adding counter project

---
+++ counter.js
@@ -0,0 +1,21 @@
+const decButton = document.getElementById('decButton')
+const incButton = document.getElementById('incButton')
+const randomButton = document.getElementById('randomButton')
+const zeroButton = document.getElementById('zeroButton')
+const spanNumber = document.getElementById('number');
+let num = 0;
+
+decButton.addEventListener('click', () => {
+    num--;
+    spanNumber.innerText = num;
+})
+
+incButton.addEventListener('click', () => {
+    num++;
+    spanNumber.innerText = num;
+})
+
+zeroButton.addEventListener('click', () => {
+    num = 0;
+    spanNumber.innerText = num;
+})
